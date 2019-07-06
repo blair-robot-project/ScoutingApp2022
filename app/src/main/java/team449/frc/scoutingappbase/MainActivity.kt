3af3afb5f06ba7dd05fda2e0ce0fb0d43df02124
Carter Wilson <carter47w@gmail.com>
2019-07-06 11:41:23 -0400

    gradle setup for binding and nav

--- app/src/main/java/team449/frc/scoutingappbase/MainActivity.kt
+++ app/src/main/java/team449/frc/scoutingappbase/MainActivity.kt
@@ -1,12 +1,15 @@
 package team449.frc.scoutingappbase
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
